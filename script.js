6dd9de2c5acc77cd11cced3a8f01b51b032755be
Anthony <perezant121@gmail.com>
2022-08-22 16:31:10 -0400

    Added variable for questions

--- script.js
+++ script.js
@@ -1,7 +1,26 @@
 var startEL = document.querySelector("#start");
-var quizEL = document.querySelector("#quiz)");
-var scoreEL = document.querySelector("#score)");
+var quizEL = document.querySelector("#quiz");
+var scoreEL = document.querySelector("#score");
 var startButton = document.querySelector("#start button");
 var submitButton = document.querySelector("#score button");
-var questionEL = document.querySelector("#question");
+var questionsEL = document.querySelector("#question");
 var answersEL = document.querySelector("#answers");
+
+var questions = [
+    {name: "First", 
+    answers: [1,2,3,4,]
+    }, 
+    {name: "Second",
+    answers: [5,6,7,8,]
+    },
+    {name: "Third",
+    answers: [9,10,11,12,]
+    },
+    {name: "Forth",
+    answers: [13,14,15,16,]
+    },
+    {name: "Fifth",
+    answers: [17,18,19,20,]
+    },
+]
+
