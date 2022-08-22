a11527f2075581f03df5fc4d821d93bbaca03de0
Anthony <perezant121@gmail.com>
2022-08-22 15:46:27 -0400

    Initial commit, added added html and javascript query selectors

---
+++ script.js
@@ -0,0 +1,7 @@
+var startEL = document.querySelector("#start");
+var quizEL = document.querySelector("#quiz)");
+var scoreEL = document.querySelector("#score)");
+var startButton = document.querySelector("#start button");
+var submitButton = document.querySelector("#score button");
+var questionEL = document.querySelector("#question");
+var answersEL = document.querySelector("#answers");
